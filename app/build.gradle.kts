ea4d552b2989ac903b74aff0e75300cb400393b3
wolinwolin111 <root@server.wolin.com>
2026-08-10 02:52:38 +0800

    build: prepare mobile app 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yueshu.clinic"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
